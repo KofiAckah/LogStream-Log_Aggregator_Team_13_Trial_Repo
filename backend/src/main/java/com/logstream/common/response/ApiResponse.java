58b7a18924b3f9ab1e11335bcf3a0d16082f5e04
Nlenjibi Timothy <nlenjibitimo@gmail.com>
2026-03-05 12:05:12 +0000

    refactor: clean up to fix bugs

--- backend/src/main/java/com/logstream/common/response/ApiResponse.java
+++ backend/src/main/java/com/logstream/common/response/ApiResponse.java
@@ -1,4 +1,4 @@
-package com.smart_ecomernce_api.smart_ecomernce_api.common.response;
+package com.logstream.common.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
